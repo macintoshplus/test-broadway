96518513962c6d45fe9d55b584135889c124c7f0
macintoshplus <jbnahan@gmail.com>
2014-09-03 22:29:41 +0200

    Make commande in default route

--- src/Jb/TestBundle/Controller/DefaultController.php
+++ src/Jb/TestBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Jb\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Jb\TestBundle\Domain\Command\Test1Command;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+
+    	$this->get('broadway.command_handling.command_bus')->dispatch(new Test1Command($name));
+
         return $this->render('JbTestBundle:Default:index.html.twig', array('name' => $name));
     }
 }
