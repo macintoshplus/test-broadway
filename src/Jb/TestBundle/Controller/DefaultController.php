d9a0dd77a4958d5e18cb344dde113002b0fe6e68
Jean-Baptiste NAHAN <jean-baptiste@nahan.fr>
2014-09-04 12:05:21 +0200

    Add route and action for update aggregate

--- src/Jb/TestBundle/Controller/DefaultController.php
+++ src/Jb/TestBundle/Controller/DefaultController.php
@@ -1,16 +1,42 @@
 <?php
+/**
+ * @author Jean-Baptiste Nahan <jean-baptiste@nahan.fr>
+ * @license MIT
+ */
 
 namespace Jb\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Jb\TestBundle\Domain\Command\Test1Command;
+use Jb\TestBundle\Domain\Command\Test2Command;
 
 class DefaultController extends Controller
 {
+	/**
+	 * Make new aggregate and save it
+	 * @param string $name
+	 * @return Response
+	 */
     public function indexAction($name)
     {
+    	//Générate new id
+    	$id = $this->get('broadway.uuid.generator')->generate();
 
-    	$this->get('broadway.command_handling.command_bus')->dispatch(new Test1Command($name));
+    	$this->get('broadway.command_handling.command_bus')->dispatch(new Test1Command($id, $name));
+
+        return $this->render('JbTestBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     *  Change texte in aggragate ID
+     * @param uuid string $id
+     * @param string $name
+	 * @return Response
+     */
+    public function changeAction($id, $name)
+    {
+
+    	$this->get('broadway.command_handling.command_bus')->dispatch(new Test2Command($id,$name));
 
         return $this->render('JbTestBundle:Default:index.html.twig', array('name' => $name));
     }
